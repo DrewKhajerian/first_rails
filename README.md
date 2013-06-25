dff2ed7dde0ca90e02a1bedc88df85022ab77d4a
DrewKhajerian <arkhajer@gmail.com>
2013-06-24 20:25:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is my first Rails App
+
+by Andrew Khajerian
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = '1d49c614c53f275591a59219866ff1e298bf9ca785f9617091ff38a1a86b1ede807ecb2496f093168f9995f01a123d36e83734a5e26c4d716b6de19e990acaa8'
