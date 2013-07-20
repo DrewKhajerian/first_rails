6c3ed40c2fa8a6545d68ec3a4afc5b849874ef19
DrewKhajerian <arkhajer@gmail.com>
2013-07-20 13:47:54 -0400

    pins association to users and paperclip gem

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,7 @@ class Pin < ActiveRecord::Base
 
   # ensures someone has to enter a description, presence: true
   validates :description, presence: true
+
+  belongs_to :user
+  validates :user_id, presence: true
 end
